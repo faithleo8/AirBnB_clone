484f160cdbd9c9eb5d4e10cbb628bc6eb3d4183e
Ochieng24 <edwine.ochi24@gmail.com>
2023-11-13 05:17:12 -0800

    modified console.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,4 @@
-[200~#!/usr/bin/python3
-        """Import the file_storage module"""
-        from models.engine.file_storage import FileStorage
-
-        storage = FileStorage()
-        storage.reload()
+"""Import the file_storage module"""
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
