803182571b83e68fb9c073f37b8a291174ccf610
Ochieng24 <edwine.ochi24@gmail.com>
2023-11-13 02:00:34 -0800

    update

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class amenity that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    class Amenity that inherits from BaseModel
+    """
+    name = ""
+
